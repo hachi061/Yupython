51616e1db66b88f2ef0b7c0270446dd03863c3c0
林芷妤 <s0961566865@gmail.com>
2025-05-15 12:40:05 +0000

    lesson6

--- lesson6/lesson6_2.py
+++ lesson6/lesson6_2.py
@@ -1,8 +1,4 @@
-def calculate_bmi(h:int | float,W:int):
-    bmicalculate = w/ h ** 2
-    return bmicalculate
-
-def main():
+if __name__ == "__ main__ ":
     try:
         height:int = int(input('請輸入你的身高(公分 cm):'))
         if height < 120 or height > 220:
@@ -11,7 +7,7 @@ def main():
         weight = eval(input('請輸入你的體重(公斤 kg):'))
         if weight < 30.0 or weight > 200.0:
             raise Exception(f'輸入的體重: {weight:.2f} 公斤 不在 30 ~ 200 範圍內')
-        bmi = calculate_bmi = (height,weight) 
+        bmicalculate = weight / height ** 2
     except ValueError:
         print('輸入發生錯誤')
     except Exception as e :
@@ -19,18 +15,16 @@ def main():
     else:
         print(f'你的身高:{height*100} 公分')
         print(f'你的體重:{weight:.2f} 公斤')
-        print(f'你的 BMI值為:{bmi:.2f}')
-        if bmi< 18.5:
+        print(f'你的 BMI值為:{bmicalculate:.2f}')
+        if bmicalculate < 18.5:
             print('你的體重過輕')
-        elif bmi < 24:
+        elif bmicalculate < 24:
             print('你的體重正常')
-        elif bmi < 27:
+        elif bmicalculate < 27:
             print('你的體重稍微過重')
-        elif bmie < 30:
+        elif bmicalculate < 30:
             print('你已經輕度肥胖')
-        elif bmie < 35:
+        elif bmicalculate < 35:
             print('你已經中度肥胖')
         else:
             print('你已經重度肥胖')
-if __name__ == "__main__":
-    main()
