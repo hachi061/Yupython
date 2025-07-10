9d969525352ac1d56aa85bb92af0500a6815fc6d
hachi061 <s0961566865@gmail.com>
2025-07-10 21:27:58 +0800

    modify

--- lesson21_web/main.py
+++ lesson21_web/main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 client = genai.Client()
 
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
-handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+handler = WebhookHandler(os.environ['0'])
 
 @app.route("/")
 @app.route("/<string:question>")
